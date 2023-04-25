f1ef350d5835722a2654c58ea0b31646cf1ad166
Stefano Baghino <stefanobaghino@users.noreply.github.com>
2023-04-25 08:51:32 +0200

    Implement the first few basic expressions (#23)
    Fixes #2

---
+++ src/main/scala/lox/AstPrinter.scala
@@ -0,0 +1,28 @@
+package lox
+
+final class AstPrinter extends Expr.Visitor[String] {
+
+  override def visitBinary(binary: Expr.Binary): String =
+    s"${parenthesize(binary.operator.lexeme, binary.left, binary.right)}"
+
+  override def visitGrouping(grouping: Expr.Grouping): String =
+    s"${parenthesize("group", grouping.expression)}"
+
+  override def visitLiteral(literal: Expr.Literal): String =
+    if (literal == null) "nil" else literal.value.toString
+
+  override def visitUnary(unary: Expr.Unary): String =
+    s"${parenthesize(unary.operator.lexeme, unary.right)}"
+
+  private def parenthesize(name: String, expressions: Expr*): String = {
+    val builder = new StringBuilder
+    builder.append("(").append(name)
+    for (expr <- expressions) {
+      builder.append(" ")
+      builder.append(expr.accept(this))
+    }
+    builder.append(")")
+    builder.toString
+  }
+
+}
