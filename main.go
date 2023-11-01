d5ea30a6d70a66fe812d27d372fd6e0a5736097e
Stefano Baghino <stefano@baghino.me>
2023-11-01 09:59:25 +0100

    Fix expected CLI args numbers

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
+	if len(os.Args) > 2 {
 		fmt.Println("Usage: lox [script]")
 		os.Exit(64)
-	} else if len(os.Args) == 1 {
-		lox.RunFile(os.Args[0])
+	} else if len(os.Args) == 2 {
+		lox.RunFile(os.Args[1])
 	} else {
 		lox.RunPrompt()
 	}
